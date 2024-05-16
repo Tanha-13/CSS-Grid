962a0668a6f85f2ee4aea285458e8e9161c38d5a
Tanha <nusrattanha897@gmail.com>
2024-05-16 19:30:48 +0600

    basic implementation of grid

---
+++ notes.js
@@ -0,0 +1,13 @@
+// Grid Container, Grid line, Grid Item, Grid Cell
+
+// topic: display: grid
+/**
+ * grid - generates a block level grid
+ * inline-grid - generates an inline level grid
+ */
+
+// topic: grid-template-columns and grid-template-rows
+/**
+ * defines the columns and rows of the grid with a space-separated list of values. The values represent the track size, and the space between them represents the grid line.
+ * 
+ */
