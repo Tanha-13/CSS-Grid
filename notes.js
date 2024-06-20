a0c996219c06f1c8dc9423af6cb7bcd3b137a098
Tanha <nusrattanha897@gmail.com>
2024-06-20 23:09:19 +0600

    revision of rows and columns

--- notes.js
+++ notes.js
@@ -2,17 +2,26 @@
 
 // topic: display: grid
 /**
- * grid - generates a block level grid
- * inline-grid - generates an inline level grid
+ * grid - generates a block level grid container
+ * inline-grid - generates an inline level grid container
  */
 
 // topic: grid-template-columns and grid-template-rows
 /**
+ * grid-template-columns specifies the number and widths of columns browsers should display in the selected grid container.
+ *     grid-template-columns: <track-sizes>
+ * track-size: can be a length(px,em,rem), percentage, fraction of available space(fr unit), auto or combination of these.
+ * 
+ * grid-template-rows specifies the number and heights of rows browsers should display in the selected grid container.
+ *     grid-template-rows: <track-size>
+ * 
+ * grid-auto-columns and grid-auto-rows
+ *
  * defines the columns(width) and rows(height) of the grid with a space-separated list of values. The values represent the track size, and the space between them represents the grid line.
  * 
- * in grid layout, we get a new flexible unit - Fr unit. Fr is a fractional unit and 1 fr is for 1 part of the available space. This unit allows to set the size of a track as a fraction of the free space of the grid container. The free space is calculated after any non-flexible items. 
+ * in grid layout, we get a new flexible unit - Fr unit(fraction of the available space). Fr is a fractional unit and 1 fr is for 1 part of the available space. This unit allows to set the size of a track as a fraction of the free space of the grid container. The free space is calculated after any non-flexible items. 
  * 
- * repeat() notation - is used to make rules more concise and easier to understand when creating a large amount of columns or rows. Fi your definition contains repeating parts, you can use the repeat() notation.
+ * repeat() notation - is used to make rules more concise and easier to understand when creating a large amount of columns or rows. It is used to repeat a track pattern. 
  *      grid-template-columns: 1fr 2fr 1fr 2fr 1fr 2fr;
         grid-template-rows: 100px auto 20% 100px auto 20%;
  * But we can write - 
